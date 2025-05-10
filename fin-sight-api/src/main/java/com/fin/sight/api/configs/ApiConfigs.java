f94182555cdc0139048661bfe183cf50de4b799e
Prakriti Panja <prakpanja2012@gmail.com>
2025-05-10 21:15:21 +0530

    FinSight UI - base structure with login component

--- fin-sight-api/src/main/java/com/fin/sight/api/configs/ApiConfigs.java
+++ fin-sight-api/src/main/java/com/fin/sight/api/configs/ApiConfigs.java
@@ -9,10 +9,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan(basePackages = {"com.fin.sight.api"})
-public class ApiConfigs {
+public class ApiConfigs implements WebMvcConfigurer {
 
     @Bean
     public JwtUtils jwtUtils(@Value("${jwt.secret.key}") String key) {
@@ -22,5 +24,14 @@ public class ApiConfigs {
     @Bean
     public UserService userService(@Autowired UserRepository userRepository, @Autowired CredentialsRepository credentialsRepository, JwtUtils jwtUtils) {
         return new UserService(userRepository, credentialsRepository, jwtUtils);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // apply to all routes
+                .allowedOrigins("http://localhost:4200") // allow Angular frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
